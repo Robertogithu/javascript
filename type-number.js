69ce1ba0c45478c21ab4084d649517055f2810cd
unknown <otrebor221@hotmail.com>
2023-09-19 16:21:58 -0300

    primeiro commit exemplos aula 1

---
+++ type-number.js
@@ -0,0 +1,18 @@
+// tipo Number
+
+const meuNumero = 3;
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuantesemZero = .5;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+const operacaopontoFlutuante = numeroPontoFlutuante + pontoFlutuantesemZero * primeiroNumero /segundoNumero;
+
+console.log(operacaopontoFlutuante);
+
+const alura = "alura";
+
+console.log(alura * meuNumero);
---
+++ type-string.js
@@ -0,0 +1,19 @@
+const texto1 = "e ai tudo certo!!!";
+
+const texto2 = 'e ai tudo certo!!';
+
+const senha = "senhamonstro##@@#@#"
+
+const StringDeNumeros = "3456789"
+
+const meuNome = "Roberto";
+
+const citacao = "Meu nome é ";
+
+// template string ou template literal
+
+// concatenção (+)
+
+
+console.log(texto1, texto2, senha);
+console.log(citacao + meuNome);
