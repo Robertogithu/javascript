01c82dfb4100e67b41fb1def64020ccde12479d4
unknown <otrebor221@hotmail.com>
2023-09-20 14:22:48 -0300

    novos arquivos

---
+++ comparacoes.js
@@ -0,0 +1,6 @@
+// == (comparação implicita)
+
+const numero = 15;
+const texto = "15";
+
+console.log(typeof numero, typeof texto);
---
+++ template-string.js
@@ -0,0 +1,7 @@
+const nome = "Roberto";
+const idade = 2023 - 1982;
+const cidadeDeNascimento  = "São Paulo";
+
+const apresentacao = (`Meu nome é ${nome} e tenho ${idade} anos e nasci na cidade de ${cidadeDeNascimento}`);
+
+console.log(apresentacao);
